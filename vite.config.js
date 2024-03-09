7e771ea3c5f19194897b562c7fa4c605d96411f1
khushba ahmed <khushba@sagbrain.com>
2024-03-10 01:40:49 +1300

    base added in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bcal-budget-app',
   loaders: [
     { test: /\.json$/, loader: 'json-loader' },
     // other loaders 
